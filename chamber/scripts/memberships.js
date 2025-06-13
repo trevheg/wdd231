9b44e28dd0abcb4e33cf021b3aa6c0a91b89361b
trevheg <trevorhegstrom@gmail.com>
2025-06-13 10:04:36 -0700

    updated join page for grader

--- chamber/scripts/memberships.js
+++ chamber/scripts/memberships.js
@@ -5,19 +5,42 @@ const membershipInfo = document.querySelector("#membershipInfoDialog div");
 
 membershipButtonGold.addEventListener("click", () => {
     dialogBox.showModal();
-    membershipInfo.innerHTML = "These are the gold membership benefits";
+    membershipInfo.innerHTML = `
+    <p>Gold members get:</p>
+    <ul>
+        <li>A listing in the membership directory</li>
+        <li>A chance of being featured in the "spotlight" listing</li>
+        <li>A reserved berth at the docks</li>
+    </ul>
+    `;
 })
 membershipButtonSilver.addEventListener("click", () => {
     dialogBox.showModal();
-    membershipInfo.innerHTML = "These are the silver membership benefits";
+    membershipInfo.innerHTML = `
+    <p>Silver members get:</p>
+    <ul>
+        <li>A listing in the membership directory</li>
+        <li>A chance of being featured in the "spotlight" listing</li>
+    </ul>
+    `;
 })
 membershipButtonBronze.addEventListener("click", () => {
     dialogBox.showModal();
-    membershipInfo.innerHTML = "These are the bronze membership benefits";
+    membershipInfo.innerHTML = `
+    <p>Bronze members get:</p>
+    <ul>
+        <li>A listing in the membership directory</li>
+    </ul>
+    `;
 })
 membershipButtonNP.addEventListener("click", () => {
     dialogBox.showModal();
-    membershipInfo.innerHTML = "These are the non-profit membership benefits";
+    membershipInfo.innerHTML = `
+    <p>Non-profit members get:</p>
+    <ul>
+        <li>A listing in the membership directory</li>
+    </ul>
+    `;
 })
 
 closeButton.addEventListener("click", () => {
